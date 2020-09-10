def905101e807b7630952a84b7503fbf6a6ed37c
Mylyn-1122 <69164770+Mylyn-1122@users.noreply.github.com>
2020-09-09 20:52:04 -0700

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -1,9 +1,10 @@
-class BaseClass{
-    constructor(x, y, width, height, angle) {
+class Box{
+    constructor(x, y, width, height) {
         var options = {
-            'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            restitution:0.8,
+            friction:0.10,
+            density:0.10,
+            isStatic:false
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
@@ -11,10 +12,9 @@ class BaseClass{
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
+        rotate(this.body.angle);
         rectMode(CENTER);
         rect(0, 0, this.width, this.height);
         pop();
